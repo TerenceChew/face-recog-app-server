4d28e8866b1884dcc4a9ddec01a51ad7b99f7dde
TerenceChew <wschew97@gmail.com>
2023-02-08 21:13:17 +0800

    Clean up code

--- server.js
+++ server.js
@@ -1,61 +1,49 @@
-const express = require('express');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const knex = require('knex');
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const knex = require("knex");
 
 // Controllers
-const clarifaiApi = require('./controllers/clarifaiApi');
-const signIn = require('./controllers/signIn');
-const register = require('./controllers/register');
-const imageSubmit = require('./controllers/imageSubmit');
+const clarifaiApi = require("./controllers/clarifaiApi");
+const signIn = require("./controllers/signIn");
+const register = require("./controllers/register");
+const imageSubmit = require("./controllers/imageSubmit");
 
 const app = express();
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
-// Test database
-// let users = [
-//   {
-//     id: 1,
-//     name: 'Jim',
-//     email: 'jim@gmail.com',
-//     rank: 0,
-//     hash: 'abc123',
-//     joined: new Date()
-//   }
-// ];
-
 // Testing
-app.get('/', (req, res) => {
-  res.json('GET success');
-})
+app.get("/", (req, res) => {
+  res.json("GET success");
+});
 
 // Endpoints listed as below
 
 // /signIn
-app.post('/signIn', signIn.handleSignIn(bcrypt, db))
+app.post("/signIn", signIn.handleSignIn(bcrypt, db));
 
 // /register
-app.post('/register', register.handleRegister(bcrypt, db))
+app.post("/register", register.handleRegister(bcrypt, db));
 
 // /clarifaiApi
-app.post('/clarifaiApi', clarifaiApi.makeApiCall)
+app.post("/clarifaiApi", clarifaiApi.makeApiCall);
 
 // /imageSubmit
-app.put('/imageSubmit', imageSubmit.handleImageSubmit(db))
+app.put("/imageSubmit", imageSubmit.handleImageSubmit(db));
 
-// Port setups 
+// Port setups
 let port = process.env.PORT;
 
 if (port == null || port == "") {
@@ -65,7 +53,6 @@ app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(`Listening to port: ${port}`)
+    console.log(`Listening to port: ${port}`);
   }
 });
-
